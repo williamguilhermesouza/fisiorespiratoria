9196332a8378a0f783a30577419880a592cf1d9b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-06-10 19:30:24 -0300

    finished resultados second page

--- src/pages/Resultados/index.js
+++ src/pages/Resultados/index.js
@@ -1,12 +1,76 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
 export default function Resultados() {
+    const navigation = useNavigation();
+
+    function navigateToDados() {
+        navigation.navigate('Dados');
+    };
+
     return (
-        <View>
-            <Text>Hello2st</Text>
+        <View style={ styles.container }>
+            <ScrollView horizontal>
+                <View style={styles.container}>
+                    <View style={styles.tableHeader}>
+                        <Text style={styles.tableHeaderText}>Aparelho</Text>
+                        <Text style={styles.tableHeaderText}>Predito</Text>
+                        <Text style={styles.tableHeaderText}>Alvo Terapêutico</Text>
+                        <Text style={{...styles.tableHeaderText, paddingTop: 0}}>Condições {'\n'} do Paciente</Text>
+                    </View>
+                    
+                    <View style={styles.tableHeader}>
+                        <Text style={styles.tableCell}>Peak Flow</Text>
+                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}> > 357,6</Text>
+                        <Text style={styles.tableCell}>Zona Verde</Text>
+                    </View>
+
+                     
+                    <View style={styles.tableHeader}>
+                        <Text style={styles.tableCell}>PIMax</Text>
+                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}> > ou = 357,6</Text>
+                        <Text style={styles.tableCell}>Zona Verde</Text>
+                    </View>
+
+                    <View style={styles.tableHeader}>
+                        <Text style={styles.tableCell}>PEMax</Text>
+                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}> > ou = 357,6</Text>
+                        <Text style={styles.tableCell}>Zona Verde</Text>
+                    </View>
+                    
+                    <View style={styles.tableHeader}>
+                        <Text style={styles.tableCell}>CI</Text>
+                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}> > ou = 357,6</Text>
+                        <Text style={styles.tableCell}>Zona Verde</Text>
+                    </View>
+ 
+                    <View style={styles.tableHeader}>
+                        <Text style={styles.tableCell}>CV</Text>
+                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}> > 357,6</Text>
+                        <Text style={styles.tableCell}>Zona Verde</Text>
+                    </View>
+
+                    <View style={styles.tableHeader}>
+                        <Text style={styles.tableCell}>P Ideal</Text>
+                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}>357,6</Text>
+                        <Text style={styles.tableCell}>Zona Verde</Text>
+                    </View>
+
+                </View>
+            </ScrollView>
+            
+            <TouchableOpacity style={ styles.button } onPress={ navigateToDados }>
+                <Text style={ styles.buttonText } >Voltar</Text>
+            </ TouchableOpacity>
         </View>
     );
 }
--- src/pages/Resultados/styles.js
+++ src/pages/Resultados/styles.js
@@ -5,5 +5,46 @@ export default StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         alignItems: 'center',
+        marginTop: 30,
+    },
+
+    tableHeader: {
+        flexDirection: 'row',
+        height: 40,
+        minWidth: 400,
+        marginLeft: 5,
+    },
+
+    tableHeaderText: {
+        backgroundColor: '#BBCEFF',    
+        marginLeft: 1,
+        minWidth: 100,
+        textAlign: 'center',
+        paddingTop: 8,
+    },
+
+    tableCell: {
+        backgroundColor: '#E3E9FB',
+        marginLeft: 1,
+        marginTop: 1,
+        minWidth: 100,
+        textAlign: 'center',
+        paddingTop: 8,
+    },
+    
+    button: {
+        width: '35%',
+        height: 50,
+        marginTop: 60,
+        borderRadius: 6,
+        alignItems: 'center',
+        backgroundColor: '#000',
+        bottom: 70,
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontSize: 18,
+        paddingTop: 8,
     },
 });

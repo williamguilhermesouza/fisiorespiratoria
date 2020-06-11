9690183824fdfc6b2ddff7e21be66959c52f11cd
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-06-11 13:25:13 -0300

    fixed values to pass to the second page

--- src/pages/Dados/index.js
+++ src/pages/Dados/index.js
@@ -1,61 +1,147 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
 export default function Dados() {
     const navigation = useNavigation();
+    let [sex, setSex] = useState('');
+    let [age, setAge] = useState(0);
+    let [height, setHeight] = useState(0.0);
+    let [weight, setWeight] = useState(0.0);
+    let [realflow, setRealflow] = useState(0.0);
+    let [realpimax, setRealpimax] = useState(0.0);
+    let [realpemax, setRealpemax] = useState(0.0);
+    let [realci, setRealci] = useState(0.0);
+    let [realcv, setRealcv] = useState(0.0);
+
+    function calculateResults() {
+        let calcflow, calcpimax, calcpemax, calcci, calccv, pideal;
+
+        calcflow = 1;
+        calcpimax = 2;
+        calcpemax = 3;
+        calcci = 4;
+        calccv = 5;
+        pideal = 0;
+
+        const results = { calcflow, calcpimax, calcpemax, calcci, calccv, pideal }
+        return results;
+    };
 
     function navigateToResultados() {
-        navigation.navigate('Resultados');
+        const results = calculateResults();
+        if (sex == 'M' || sex == 'F') {
+            navigation.navigate('Resultados', {
+                realflow,
+                realpimax,
+                realpemax,
+                realci,
+                realcv,
+                calcflow: results.calcflow,
+                calcpimax: results.calcpimax,
+                calcpemax: results.calcpemax,
+                calcci: results.calcci,
+                calccv: results.calccv,
+                weight,
+                pideal: results.pideal
+            });
+        }
     };
 
     return (
         <View style={ styles.container }>
             <View style={ styles.inputView }>
                 <Text style={ styles.inputText}>Sexo</ Text>
-                <TextInput style={ styles.textInput } placeholder='M/F' />
+                <TextInput 
+                    style={ styles.textInput } 
+                    placeholder='M/F'
+                    maxLength={1}
+                    onChangeText={ text => setSex(text) }
+                    onSubmitEditing={ (sex != 'M' && sex != 'F' && sex != '') ? Alert.alert('Sexo deve ser M ou F!!!') : console.log(sex) }
+                    
+                />
             </ View>
 
             <View style={ styles.inputView }>
                 <Text style={ styles.inputText}>Idade</ Text>
-                <TextInput style={ styles.textInput } placeholder='anos' />
+                <TextInput 
+                    style={ styles.textInput } 
+                    placeholder='anos' 
+                    keyboardType='decimal-pad' 
+                    onChangeText={ text => setAge(text) }
+                />
             </ View>
 
             <View style={ styles.inputView }>
                 <Text style={ styles.inputText}>Altura</ Text>
-                <TextInput style={ styles.textInput } placeholder='cm' />
+                <TextInput 
+                    style={ styles.textInput } 
+                    placeholder='cm' 
+                    onChangeText={ text => setHeight(text) }
+                    keyboardType='decimal-pad'
+                />
             </ View>
 
             <View style={ styles.inputView }>
                 <Text style={ styles.inputText}>Peso</ Text>
-                <TextInput style={ styles.textInput } placeholder='Kg' />
+                <TextInput 
+                    style={ styles.textInput } 
+                    placeholder='Kg'
+                    onChangeText={ text => setWeight(text) }
+                    keyboardType='decimal-pad'
+                />
             </ View>
 
             <View style={ styles.inputView }>
                 <Text style={ styles.inputText}>Peak Flow</ Text>
-                <TextInput style={ styles.textInput } placeholder='L/minuto' />
+                <TextInput 
+                    style={ styles.textInput } 
+                    placeholder='L/minuto' 
+                    onChangeText={ text => setRealflow(text) }
+                    keyboardType='decimal-pad'
+                />
             </ View>
 
             <View style={ styles.inputView }>
                 <Text style={ styles.inputText}>PIMax</ Text>
-                <TextInput style={ styles.textInput } placeholder='cm.H2O' />
+                <TextInput 
+                    style={ styles.textInput } 
+                    placeholder='cm.H2O' 
+                    onChangeText={ text => setRealpimax(text) }
+                    keyboardType='decimal-pad'
+                />
             </ View>
 
             <View style={ styles.inputView }>
                 <Text style={ styles.inputText}>PEMax</ Text>
-                <TextInput style={ styles.textInput } placeholder='cm.H2O' />
+                <TextInput 
+                    style={ styles.textInput } 
+                    placeholder='cm.H2O' 
+                    onChangeText={ text => setRealpemax(text) }
+                    keyboardType='decimal-pad'
+                />
             </ View>
 
             <View style={ styles.inputView }>
                 <Text style={ styles.inputText}>CI</ Text>
-                <TextInput style={ styles.textInput } placeholder='ml' />
+                <TextInput 
+                    style={ styles.textInput } 
+                    placeholder='ml'
+                    onChangeText={ text => setRealci(text) }
+                    keyboardType='decimal-pad'
+                />
             </ View>
 
             <View style={ styles.inputView }>
                 <Text style={ styles.inputText}>CV</ Text>
-                <TextInput style={ styles.textInput } placeholder='ml/Kg' />
+                <TextInput 
+                    style={ styles.textInput } 
+                    placeholder='ml/Kg'
+                    onChangeText={ text => setRealcv(text) }
+                    keyboardType='decimal-pad'
+                />
             </ View>
 
             <TouchableOpacity style={ styles.button } onPress={ navigateToResultados }>
--- src/pages/Resultados/index.js
+++ src/pages/Resultados/index.js
@@ -4,8 +4,22 @@ import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
-export default function Resultados() {
+export default function Resultados({ route }) {
     const navigation = useNavigation();
+    const { 
+        realflow, 
+        realpimax, 
+        realpemax, 
+        realci, 
+        realcv,
+        calcflow,
+        calcpimax,
+        calcpemax,
+        calcci,
+        calccv,
+        weight,
+        pideal 
+    } = route.params;
 
     function navigateToDados() {
         navigation.navigate('Dados');
@@ -17,6 +31,7 @@ export default function Resultados() {
                 <View style={styles.container}>
                     <View style={styles.tableHeader}>
                         <Text style={styles.tableHeaderText}>Aparelho</Text>
+                        <Text style={styles.tableHeaderText}>Paciente</Text>
                         <Text style={styles.tableHeaderText}>Predito</Text>
                         <Text style={styles.tableHeaderText}>Alvo Terapêutico</Text>
                         <Text style={{...styles.tableHeaderText, paddingTop: 0}}>Condições {'\n'} do Paciente</Text>
@@ -24,7 +39,8 @@ export default function Resultados() {
                     
                     <View style={styles.tableHeader}>
                         <Text style={styles.tableCell}>Peak Flow</Text>
-                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}>{realflow}</Text>
+                        <Text style={styles.tableCell}>{calcflow}</Text>
                         <Text style={styles.tableCell}> > 357,6</Text>
                         <Text style={styles.tableCell}>Zona Verde</Text>
                     </View>
@@ -32,36 +48,41 @@ export default function Resultados() {
                      
                     <View style={styles.tableHeader}>
                         <Text style={styles.tableCell}>PIMax</Text>
-                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}>{realpimax}</Text>
+                        <Text style={styles.tableCell}>{calcpimax}</Text>
                         <Text style={styles.tableCell}> > ou = 357,6</Text>
                         <Text style={styles.tableCell}>Zona Verde</Text>
                     </View>
 
                     <View style={styles.tableHeader}>
                         <Text style={styles.tableCell}>PEMax</Text>
-                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}>{realpemax}</Text>
+                        <Text style={styles.tableCell}>{calcpemax}</Text>
                         <Text style={styles.tableCell}> > ou = 357,6</Text>
                         <Text style={styles.tableCell}>Zona Verde</Text>
                     </View>
                     
                     <View style={styles.tableHeader}>
                         <Text style={styles.tableCell}>CI</Text>
-                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}>{realci}</Text>
+                        <Text style={styles.tableCell}>{calcci}</Text>
                         <Text style={styles.tableCell}> > ou = 357,6</Text>
                         <Text style={styles.tableCell}>Zona Verde</Text>
                     </View>
  
                     <View style={styles.tableHeader}>
                         <Text style={styles.tableCell}>CV</Text>
-                        <Text style={styles.tableCell}>447</Text>
+                        <Text style={styles.tableCell}>{realcv}</Text>
+                        <Text style={styles.tableCell}>{calccv}</Text>
                         <Text style={styles.tableCell}> > 357,6</Text>
                         <Text style={styles.tableCell}>Zona Verde</Text>
                     </View>
 
                     <View style={styles.tableHeader}>
-                        <Text style={styles.tableCell}>P Ideal</Text>
-                        <Text style={styles.tableCell}>447</Text>
-                        <Text style={styles.tableCell}>357,6</Text>
+                        <Text style={styles.tableCell}>Peso Ideal</Text>
+                        <Text style={styles.tableCell}>{weight}</Text>
+                        <Text style={styles.tableCell}>{pideal}</Text>
+                        <Text style={styles.tableCell}>{weight-pideal}</Text>
                         <Text style={styles.tableCell}>Zona Verde</Text>
                     </View>
 

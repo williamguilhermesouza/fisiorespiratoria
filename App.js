ef67d79e5736e06ab3ee300b249d09d7c984d81f
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-10-15 17:22:43 -0300

    Improved splash screen and added timeout

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Routes from './src/routes';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 3000);
 
 export default function App() {
   return (
